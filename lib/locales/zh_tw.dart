2ed11fb7c887b83cc27a87abd5d9ce26b8c32e52
crlambda <65450125+crlambda@users.noreply.github.com>
2021-01-27 15:26:51 +0800

    Create zh_tw.dart

---
+++ lib/locales/zh_tw.dart
@@ -0,0 +1,54 @@
+Map<String, String> get getZhTw => {
+      'CONNECT': '連結',
+      'Connect devices to the same network - use Wi-Fi or Mobile Hotspot':
+          '將裝置連結到同一個網路 - 使用 Wi-Fi 或是行動熱點',
+      'SEND': '傳送',
+      'Select any file you like': '選擇您想要傳送的任何檔案',
+      'RECEIVE': '接收',
+      'Open given link on another device in any browser':
+          '在另一裝置的任意瀏覽器開啟給定的鍵結',
+      'DONE': '完成',
+      'NEXT': '下一步',
+      'loading...': '載入中',
+      'Mobile Hotspot': '行動熱點',
+      'Connect to': '連接至',
+      'or set up a': '或設立一個',
+      ' Mobile Hotspot': '行動熱點',
+      'Not connected': '未連接',
+      'Now open this link': '現在打開此連結',
+      'in any browser': '於任何瀏覽器中',
+      'The recipient needs to be connected': '接收者必須連接',
+      'to the same network': '至同一個網路',
+      'Select file': '選擇檔案',
+      'Latest': '最近檔案',
+      'Undefined': '未定義',
+      'EVERYWHERE': '任何地方',
+      'Sharik is available for Android, Windows, MacOS and Linux!':
+          'Sharik 可在Android, Windows, MacOS 及 Linux上取得!',
+      'Click here to learn more': '輕觸此處以了解更多',
+      'Copied to Clipboard': 複製到剪貼簿',
+      'Type some text': '輸入一些文字',
+      'Close': '關閉',
+      'Send': '傳送',
+      'Text': '文字',
+      'App': '應用程式',
+      'Hide system apps': '隱藏系統應用程式',
+      'Hide non-launchable apps': '隱藏不可啟動的程式',
+      'Search': '搜尋',
+      //v2.1
+      'Updates': '更新',
+      'The latest version is already installed': '最新版本已安裝',
+      'Current version': '目前版本',
+      'The latest version': '最新版本',
+      'Changelog': '更新日誌',
+
+      //v2.2
+      'Receiver': '接收者',
+
+      //v2.4 (fonts)
+      'Andika': 'Andika',
+      'Comfortaa': 'Comfortaa',
+
+      //v2.5 (ios)
+      'Gallery': '相簿'
+    };
